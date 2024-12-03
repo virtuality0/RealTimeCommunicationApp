33867a514be2f1f775ec1576c702aa936c029b86
virtuality0 <ayushjuyal246@gmail.com>
2024-12-03 12:51:16 +0530

    added initial user setup

---
+++ RealTimeCommunicationApp/Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealTimeCommunicationApp.DAL.Models.RequestModels;
+
+namespace RealTimeCommunicationApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterAsync(RegisterUserRequestModel request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RealTimeCommunicationApp/Services/Implementations/UserService.cs
@@ -0,0 +1,14 @@
+﻿using RealTimeCommunicationApp.DAL.Models.RequestModels;
+using RealTimeCommunicationApp.DAL.Models.ResponseModels;
+using RealTimeCommunicationApp.Services.Interfaces;
+
+namespace RealTimeCommunicationApp.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        public Task<RegisterUserResponseModel> RegisterAsync(RegisterUserRequestModel response)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
